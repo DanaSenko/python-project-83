d4aade2eda438b920b7da1a10f47f04b42a97336
Dana Senko <dana.es@mail.ru>
2025-02-18 17:39:51 +0300

    update urls_cheks table

--- database.sql
+++ database.sql
@@ -6,15 +6,11 @@ CREATE TABLE IF NOT EXISTS urls (
 );
 
 CREATE TABLE IF NOT EXISTS url_checks (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id BIGINT NOT NULL,
-    status_code INT NOT NULL,
-    h1 VARCHAR(255),
-    title text,
-    description text,
-    created_at DATE DEFAULT CURRENT_DATE,
-    CONSTRAINT fk_url
-        FOREIGN KEY (url_id)
-        REFERENCES urls (id)
-        ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER NOT NULL REFERENCES urls(id) ON DELETE CASCADE,
+    status_code INTEGER,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
