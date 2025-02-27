74e4d8c04cc6641741080023cf682e156941b7f4
Dana Senko <dana.es@mail.ru>
2025-02-27 14:01:16 +0300

    add database.sql file

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS urls (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER REFERENCES urls(id) ON DELETE CASCADE,
+    status_code INTEGER,
+    h1 varchar(255),
+    title varchar(255),
+    description varchar(255),
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
+);
