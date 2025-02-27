031a756237b7c3f5115004b30cf680551c3b8ba8
Dana Senko <dana.es@mail.ru>
2025-02-27 17:07:06 +0300

    fix

--- database.sql
+++ database.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS urls (
+CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     created_at DATE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS url_checks (
+CREATE TABLE url_checks (
     id SERIAL PRIMARY KEY,
-    url_id INTEGER REFERENCES urls(id) ON DELETE CASCADE,
+    url_id INTEGER,
     status_code INTEGER,
     h1 varchar(255),
     title varchar(255),
